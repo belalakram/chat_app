3d8dfc1d543b34719f0bd6dd979fb171f61497ea
belal <20194944@fue.edu.eg>
2024-10-15 22:13:25 +0300

    add system design data (styles)

--- lib/core/themes/colors.dart
+++ lib/core/themes/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract class ColorApp{
-  static const primaryColr= Color(0xFF128C7E);
+  static const primaryColor= Color(0xFF128C7E);
   static const secondaryColor = Color(0xFF25D366);
 }
